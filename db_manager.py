e2ff198d3b88c0353b7842b827f9dfaaf71b4ce1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-03-23 10:21:14 -0300

    write connection and the create initial tables statement on __init__

--- db_manager.py
+++ db_manager.py
@@ -2,15 +2,33 @@ import sqlite3
 from sqlite3 import Error
 
 class DB_Manager:
-    def __init__(self):
-        self.conn = None
-
-    def create_connection(self, db_file="database.db"):
+    def __init__(self, db_file="database.db"):
         try:
+            # create connection
             self.conn = sqlite3.connect(db_file)
         except Error as e:
             print(e)
-    
+
+        # create projects table
+        self.create_table(""" CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """)
+
+        # create tasks table
+        self.create_table("""CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );""")
+
     def create_table(self, create_table_sql):
         """ create a table from the create_table_sql statement
         :param conn: Connection object
